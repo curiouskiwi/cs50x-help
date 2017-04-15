c0d2ef22175e5bebeba14cce88872e6f1ff009c9
Brenda Anderson <curiouskiwi@users.noreply.github.com>
2017-04-15 13:01:43 +1200

    create crypt.c
    crypt a password given a salt
     *   usage: ./crypt password salt where salt is a 2-char string
     *    used for creating hashed passwords for testing ./crack (pset2)

---
+++ crypt.c
@@ -0,0 +1,22 @@
+/**
+ *   crypt a password given a salt
+ *   usage: ./crypt password salt where salt is a 2-char string
+ *    used for creating hashed passwords for testing ./crack (pset2)
+ *   brenda anderson 
+ *   14 april 2017
+ *
+**/
+#define _XOPEN_SOURCE  
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[])
+{
+    if (argc !=3)
+    {
+        printf("usage ./crypt password salt\n");
+        return -1;
+    }
+    printf("%s\n", crypt(argv[1],argv[2]));
+    return 0;
+}
