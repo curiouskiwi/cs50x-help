173fe4cec9038c13370f4935876fa04ef7c23de3
Brenda Anderson <curiouskiwi@users.noreply.github.com>
2021-11-25 10:24:22 +1300

    Create lookup-finance.py
    Hard coded stock prices for testing CS50 Finance without using the API 
    Useful for testing scenarios of pricing/formatting with certainty

---
+++ lookup-finance.py
@@ -0,0 +1,14 @@
+# Hard coded stock for testing CS50 Finance without using the API 
+# Useful for testing scenarios of pricing/formatting with certainty 
+
+def lookup():
+
+    if symbol == "AAAA":
+        return {"name": "Test A", "price": 28.00, "symbol": "AAAA"}
+    if symbol == "BBBB":
+        return {"name": "Test B", "price": 3.27, "symbol": "BBBB"}
+    if symbol == "CCCC":
+        return {"name": "Test C", "price": 35.30, "symbol": "CCCC"}
+    if symbol == "DDDD":
+        return {"name": "Test D", "price": 0.28, "symbol": "DDDD"}
+       
